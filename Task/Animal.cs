83cd9ad94002dba3921b1f86ef51c62ea5401063
lllypik <blesphoton@gmail.com>
2022-07-21 21:50:55 +0300

    Добавьте файлы проекта.

---
+++ Task/Animal.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Task
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+
+    class Cat : Animal
+    {
+        bool wool;
+        string speech;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Cat(string name, string speech, bool wool)
+            : base(name)
+        {
+            this.speech = speech;
+            this.wool = wool;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine(speech);
+        }
+    }
+
+    class Dog : Animal
+    {
+        string speech;
+        string ears;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public Dog(string name, string speech, string ears)
+            : base(name)
+        {
+            this.speech = speech;
+            this.ears = ears;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine(speech);
+        }
+
+    }
+}
+
---
+++ Task/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat_1 = new Cat("Барсик", "Мяу", true);
+            Dog dog_1 = new Dog("Жучка", "Гав", "Длинные");
+            cat_1.ShowInfo();
+            dog_1.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
